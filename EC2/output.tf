d7244a952e68d0b02660bd037ca9d17e4501ca68
VineetKhisty <vineetk2008@gmail.com>
2025-08-28 16:50:46 +0530

    Edited output file

--- EC2/output.tf
+++ EC2/output.tf
@@ -1,16 +1,7 @@
 output "ec2_public_ip" {
-  value = aws_instance.my_ec2.public_ip
-}
-output "ec2_instance_id" {
-  value = aws_instance.my_ec2.id
-}
-output "ec2_instance_arn" {
-  value = aws_instance.my_ec2.arn
-}
-output "ec2_instance_state" {
-  value = aws_instance.my_ec2.instance_state
+  value = aws_instance.ec2_instance.public_ip
 }
 
 output "ec2_instance_public_dns" {
-  value = aws_instance.my_ec2.public_dns
+  value = aws_instance.ec2_instance.public_dns
 }
